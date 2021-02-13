2c26a8c8dfa3ffbc0e5998ca14500d516bfc5e25
sec <sec@sec-void.station>
2021-02-13 20:25:13 +0100

    Added Void

---
+++ void/gofetch.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+func do(cmd string, args ...string) string {
+	out, err := exec.Command(cmd, args...).Output()
+
+	if err != nil {
+		panic(err)
+	}
+
+	return strings.Replace(string(out), "\n", "", 1)
+
+}
+
+func main() {
+
+	user := do("whoami")
+	host := do("cat", "/etc/hostname")
+	osname := "Void"                 // might add other systems in the future
+	kernel := do("uname", "-sr")
+	uptime := do("uptime", "-p")[3:]
+	packages := strings.Count(do("xbps-query", "-l"), "\n")
+	shell := do("basename", os.Getenv("SHELL"))
+	WM := os.Getenv("WM")
+
+	// __--COLORS--__
+	reset := "\033[0m"
+
+	// ample/coffee theme
+	c1 := "\033[0m\033[1m\033[38;2;192;103;9m" // orange
+	c2 := "\033[0m\033[1m\033[38;2;226;78;78m" // red
+	font1 := "\033[0m\033[1m\033[38;2;226;78;78m"
+	font2 := "\033[0m\033[1m\033[38;2;192;103;9m"
+	// ample/coffee theme
+
+	// --__COLORS__--
+
+	/*
+
+	*/
+
+	fmt.Printf(
+`      %s_______      %s%s%s@%s%s                 
+   %s_ \______ -     %sOS:%s        %s  
+  %s| \  %s___%s  \ |    %sKERNEL:%s    %s 
+  %s| | %s/   \%s | |    %sUPTIME:%s    %s
+  %s| | %s\___/%s | |    %sPACKAGES:%s  %d
+  %s| \______ \_|    %sSHELL:%s     %s
+   %s-_______\       %sWM:%s        %s%s         
+
+`,
+		c1, font2, user, font1, font2, host,
+		c1, font1, reset, osname,
+		c1, c2, c1, font1, reset, kernel,
+		c1, c2, c1, font1, reset, uptime,
+		c1, c2, c1, font1, reset, packages,
+		c1, font1, reset, shell,
+		c1, font1, reset, WM, reset)
+
+}
+
